ad7ba5a7723c5b1b67f1343fa61ca444a59ec723
Nanret123 <nanretgungshik@gmail.com>
2023-10-22 01:08:17 +0100

    working on images

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -15,6 +15,7 @@ import { useNavigate } from "react-router-dom";
 
 
 
+
 const UserWidget = ({ userId, picturePath }) => {
     const [user, setUser] = useState(null);
     const { palette } = useTheme();
@@ -118,7 +119,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
                 <FlexBetween gap="1rem" mb="0.5rem">
                     <FlexBetween gap="1rem">
-                        <img src="../../assets/twitter.png" alt="twitter" />
+                        <img src="/assets/twitter.png" alt="twitter" />
                         <Box>
                             <Typography color={main} fontWeight="500">Twitter</Typography>
                             <Typography color={medium}>Social Network</Typography>
@@ -129,7 +130,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
                 <FlexBetween gap="1rem">
                     <FlexBetween gap="1rem">
-                        <img src="../../assets/linkedin.png" alt="linkedin" />
+                        <img src="/assets/linkedin.png" alt="linkedin" />
                         <Box>
                             <Typography color={main} fontWeight="500">Linkedin</Typography>
                             <Typography color={medium}>Network Platform</Typography>
