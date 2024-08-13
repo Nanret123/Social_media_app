d17ec454f16c1cf3e819bee04f2d37efc5ced7cd
nanret_dev <90003500+Nanret123@users.noreply.github.com>
2024-08-13 14:07:54 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,13 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
-app.use(cors());
+const corsOptions = {
+    origin: 'https://social-media-app-jet.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*FILE STORAGE */
